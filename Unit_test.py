4d1ae52806be2a607bb1b77c84ab26949afa1d5b
Zhangyu Wan <wanzy@bu.edu>
2019-02-22 23:52:10 -0500

    add unit test

---
+++ Unit_test.py
@@ -0,0 +1,19 @@
+import main
+from math import isclose
+def test_duration():
+    fnin = 'video.mp4'
+    fnout1 = '480pvideo.mp4'
+    fnout2 = '720pvideo.mp4'
+
+    orig_meta = main.ffprobe(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
+    test = main.MyProcess()
+    test.convert()
+    meta_480 = main.ffprobe(fnout1)
+    meta_720 = main.ffprobe(fnout2)
+    duration_480 = float(meta_480['streams'][0]['duration'])
+    duration_720 = float(meta_720['streams'][0]['duration'])
+    assert isclose(orig_duration, duration_480, abs_tol=1)
+    assert isclose(orig_duration, duration_720, abs_tol=1)
+    print('all successful!')
+test_duration()
--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import subprocess
-import threading
-from queue import Queue
+import multiprocessing 
+import os
+import json
 
-class MyThread(threading.Thread):
-	def __init__(self, queue):
-		threading.Thread.__init__(self)
-		self.queue = queue
+class MyProcess:
 
-	def run(self):
-		while not self.queue.empty():
-			print('there are ', threading.activeCount(), 'threads running')
-			task = self.queue.get()
-			subprocess.call(task)
-			self.queue.task_done
-			print("mission", task, 'finished')
+	def run(self, inputfile):
+		file_name_list = inputfile.split('.')
+		task = ['ffmpeg', '-i', inputfile,
+	  		'-r', '30', '-s', 'hd480', '-b:v', '1024k', '-loglevel', 'quiet', file_name_list[0]+'_480p'+'.'+file_name_list[-1],
+			'-r', '30', '-s', 'hd720', '-b:v', '2048k', '-loglevel', 'quiet', file_name_list[0]+'_720p'+'.'+file_name_list[-1]]
+		subprocess.call(task)
 
-
-def main():
-	task_queue = Queue()
-	for i in range(5):
-		name = ['480p_'+str(i)+'.mp4', '720p_'+str(i)+'.mp4']
-		task = ['ffmpeg', '-i', 'video.mp4',
-		  		'-r', '30', '-s', 'hd480', '-b:v', '1024k', '-loglevel', 'quiet', name[0],
-				'-r', '30', '-s', 'hd720', '-b:v', '2048k', '-loglevel', 'quiet', name[1]]
-		task_queue.put(task)
-	
-	while True:
-		thread_convert = MyThread(task_queue)
-		thread_convert.start()
-		if task_queue.empty():
-			print('all done')
-			break
-			
+		print("mission complete")
 
 
-if __name__ == '__main__':
-	main()
+	def convert(self):
+
+		task_pool = multiprocessing.Pool()
+		Files = os.listdir()
+		for file in Files: 
+			file_name_list = file.split('.')
+			if file_name_list[-1] == 'mp4' or file_name_list[-1] == 'mov':
+				task_pool.apply_async(self.run, args=(file,))
+		
+		task_pool.close()
+		task_pool.join()
+
+
+def ffprobe(file_name) -> dict:
+	""" get media metadata """
+	meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                	'-print_format', 'json',
+                                	'-show_streams',
+                                	'-show_format',
+                                	file_name])
+	return json.loads(meta)
+
