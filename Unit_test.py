a061f66f7b15bf363bac750232037cf5d7ad2f03
Zhangyu Wan <wanzy@bu.edu>
2019-02-25 01:52:26 -0500

    update some new feature

--- Unit_test.py
+++ Unit_test.py
@@ -2,8 +2,8 @@ import main
 from math import isclose
 def test_duration():
     fnin = 'video.mp4'
-    fnout1 = '480pvideo.mp4'
-    fnout2 = '720pvideo.mp4'
+    fnout1 = 'video_480p.mp4'
+    fnout2 = 'video_720p.mp4'
 
     orig_meta = main.ffprobe(fnin)
     orig_duration = float(orig_meta['streams'][0]['duration'])
@@ -16,4 +16,5 @@ def test_duration():
     assert isclose(orig_duration, duration_480, abs_tol=1)
     assert isclose(orig_duration, duration_720, abs_tol=1)
     print('all successful!')
+
 test_duration()
--- main.py
+++ main.py
@@ -1,39 +1,79 @@
 import subprocess
-import multiprocessing 
+import multiprocessing
 import os
 import json
+import time
 
 class MyProcess:
+    '''
+    class for converting the videos
+    '''
+    def run(self, inputfile):
+        # get all the video file name
+        file_name_list = inputfile.split('.')
+        task = ['ffmpeg',
+                '-i',
+                inputfile,
+                '-r',
+                '30',
+                '-s',
+                'hd480',
+                '-b:v',
+                '1024k',
+                '-loglevel',
+                'quiet',
+                file_name_list[0] + '_480p' + '.' + file_name_list[-1],
+                '-r',
+                '30',
+                '-s',
+                'hd720',
+                '-b:v',
+                '2048k',
+                '-loglevel',
+                'quiet',
+                file_name_list[0] + '_720p' + '.' + file_name_list[-1]]
+        subprocess.call(task)
 
-	def run(self, inputfile):
-		file_name_list = inputfile.split('.')
-		task = ['ffmpeg', '-i', inputfile,
-	  		'-r', '30', '-s', 'hd480', '-b:v', '1024k', '-loglevel', 'quiet', file_name_list[0]+'_480p'+'.'+file_name_list[-1],
-			'-r', '30', '-s', 'hd720', '-b:v', '2048k', '-loglevel', 'quiet', file_name_list[0]+'_720p'+'.'+file_name_list[-1]]
-		subprocess.call(task)
+        print("mission complete")
 
-		print("mission complete")
+    def convert(self):
+        # create a processing pool
+        task_pool = multiprocessing.Pool()
+        Files = os.listdir()
+
+        for file in Files:
+            file_name_list = file.split('.')
+            if file_name_list[-1] == 'mp4' or file_name_list[-1] == 'mov':
+                task_pool.apply_async(self.run, args=(file,))
+
+        self.get_process_info(task_pool)
+        task_pool.close()
+        task_pool.join()
+
+    def get_process_info(self,task_pool):
+
+        print("There are {} Cpus in this machine.".format(multiprocessing.cpu_count()))
+        while True:
+            print('All living children of current process: ', multiprocessing.active_children())
+            process_num = len(task_pool._cache)
+            print("There are currently {} processes running.".format(process_num))
+            if (process_num==0):
+                break
+            time.sleep(5)
 
 
-	def convert(self):
+def ffprobe(file_name)->dict:
+    """ get media metadata """
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    file_name])
+    return json.loads(meta)
 
-		task_pool = multiprocessing.Pool()
-		Files = os.listdir()
-		for file in Files: 
-			file_name_list = file.split('.')
-			if file_name_list[-1] == 'mp4' or file_name_list[-1] == 'mov':
-				task_pool.apply_async(self.run, args=(file,))
-		
-		task_pool.close()
-		task_pool.join()
+def main():
+    test = MyProcess()
+    test.convert()
 
-
-def ffprobe(file_name) -> dict:
-	""" get media metadata """
-	meta = subprocess.check_output(['ffprobe', '-v', 'warning',
-                                	'-print_format', 'json',
-                                	'-show_streams',
-                                	'-show_format',
-                                	file_name])
-	return json.loads(meta)
-
+if __name__ == '__main__':
+    main()
